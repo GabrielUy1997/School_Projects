8413ed46d181c27a2631760a179bb75f4ba2ce01
gabriel uy <guy@ramapo.edu>
2019-03-06 13:04:44 -0500

    Test commit for a C# Time program

---
+++ Lab6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time2_6 t = new Time2_6();  // calls Time1 constructor
+            string output;
+
+            // assign string representation of time to output
+            output = "Initial universal time is: " +
+               t.ToUniversalString() +
+               "\nInitial standard time is: " +
+               t.ToStandardString();
+
+            // attempt valid time settings
+            t.Hour = 13;
+            t.Minutes = 27;
+            t.Seconds = 6;
+
+            // append new string representations of time to output
+            output += "\n\nUniversal time after SetTime is: " +
+               t.ToUniversalString() +
+               "\nStandard time after SetTime is: " +
+               t.ToStandardString();
+
+            // attempt invalid time settings
+            t.Hour = 99;
+            t.Minutes = 99;
+            t.Seconds = 99;
+
+            output += "\n\nAfter attempting invalid settings: " +
+               "\nUniversal time: " + t.ToUniversalString() +
+               "\nStandard time: " + t.ToStandardString();
+
+            // MessageBox.Show(output, "Testing Class Time1");
+        }
+    }
+}
---
+++ Lab6/Time2_6.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Time2_6
+    {
+        private uint time;
+        public Time2_6() { time = 0; }
+
+        public uint Hour
+        {
+            get
+            {
+                return time / 3600;
+            }
+            set
+            {
+                if (value > 23)
+                {
+                    throw new Exception("Invalid Hour");
+                }
+                time = value * 3600 + Minutes * 60 + Seconds;
+
+            }
+        }
+        public uint Minutes
+        {
+            get
+            {
+                return time % 3600 / 60;
+            }
+            set
+            {
+                if (value > 59)
+                {
+                    throw new Exception("Invalid Minutes");
+                }
+                time = Hour * 3600 + value * 60 + Seconds;
+
+            }
+        }
+        public uint Seconds
+        {
+            get
+            {
+                return time % 60;
+            }
+            set
+            {
+                if (value > 59)
+                {
+                    throw new Exception("Invalid Seconds");
+                }
+                time = Hour * 3600 + Minutes * 60 + value;
+            }
+        }
+
+        public string ToUniversalString()
+        {
+            return String.Format(
+               "{0:D2}:{1:D2}:{2:D2}", Hour, Minutes, Seconds);
+        }
+        public string ToStandardString()
+        {
+            return String.Format("{0}:{1:D2}:{2:D2} {3}",
+               ((Hour == 12 || Hour == 0) ? 12 : Hour % 12),
+               Minutes, Seconds, (Hour < 12 ? "AM" : "PM"));
+        }
+    }
+}
